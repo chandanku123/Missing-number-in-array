fac53d789f28fe9daaa1ae3638c5c3db56563af2
Chandanku123 <chandanpandeyprog@gmail.com>
2022-08-26 00:16:40 +0530

    Missing number array

---
+++ MissingNumber.cs
@@ -0,0 +1,14 @@
+﻿internal class MissingNumber
+{
+    private static void Main(string[] args)
+    {
+        int [] arr ={1,2,3,5,6};
+        int n = arr.Length;
+       int total = (n + 1) * (n + 2) / 2;
+
+        for (int i = 0; i < n; i++)
+            total -= arr[i];
+           System.Console.WriteLine(total);
+        
+    }
+}
